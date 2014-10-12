e48a1005b58352f93bfec84ddb37d88ca63c8f88
amdenley <amdenley@users.noreply.github.com>
2014-10-12 16:36:47 +0100

    added writeline to check subtasks

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -13,7 +13,7 @@ namespace GTDTestConsole
       Task task = new Task("need to do task 1");
       Task task2 = new Task("need to do task 2");
       task.subTasks.Add(task2);
-      
+      Console.WriteLine("added task2 to task1");
     }
   }
 }
